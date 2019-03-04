9964fdace05bd73376afe7926c74bb6777e8e393
Luca Braglia <lbraglia@gmail.com>
2019-03-04 15:17:12 +0100

    update

--- R/lin_rho.R
+++ R/lin_rho.R
@@ -35,6 +35,40 @@ lin_rho <- function(x = NULL,
     setNames(rval$rho.c, c("estimate", "lower", "upper"))
 }
 
+#' Scatterplot with (0,0) to (1,1) line
+#'
+#' @param x generally the gold standard (on x axis)
+#' @param y generally the testing measurement (on y axis)
+#' @param stat which statistics to plot in a (topleft-placed) legend
+#' 
+#' @export
+lin_plot <- function(x, y, stat = c("ci", 'coef', 'none'), ...){
+    ## pch = 20
+    stat <- match.arg(stat)
+    mi <- min(c(x, y), na.rm = TRUE)
+    ma <- max(c(x, y), na.rm = TRUE)
+    xylim <- c(floor(mi), ceiling(ma))
+    lin <- lin_rho(x = x, y = y)
+    lin_string <- switch(stat,
+                         ci = sprintf("CCC: %.2f (95%% CI: %.2f - %.2f)",
+                                      lin$estimate,
+                                      lin$lower,
+                                      lin$upper),
+                         coef = sprintf("CCC: %.2f", lin$estimate))
+
+    plot(x = x, y = y,
+         xlim = xylim, ylim = xylim,
+         las = 1, pch = 20,
+         ...)
+    graphics::grid()
+    if (stat %in% c("ci", "coef"))
+        legend("topleft", legend = lin_string, bg = 'white')
+    abline(a = 0, b = 1, col = "blue")
+}
+
+
+
+
 #' OCCC (overall concordance correlation coefficient)
 #'
 #' da citare barnhart2002occc
